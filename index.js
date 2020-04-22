1baff06094eafb1890f5da770e0fc07b257470b5
Erin Chung <erinchung@Erins-MacBook-Pro.local>
2020-04-22 02:42:34 -0600

    test req header3

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const port = process.env.PORT || 8000
 const server = http.createServer((req,res)=> {
 
     const { query, pathname } = url.parse(req.url, true)
-    console.log('proto',   req.headers.x-forwarded-proto);
+    console.log('proto',   req.headers['x-forwarded-proto']);
 
 
     //overview page
